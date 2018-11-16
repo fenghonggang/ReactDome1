242b05ce7c46e837e6f59f48d7281b27c297a365
fenghonggang <18700993270@163.com>
2018-11-16 17:29:05 +0800

    代码优化

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
+          <a className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
